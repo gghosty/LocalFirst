9ddfd768e99cfedcf462028dfa4c5d1a1085974c
Casper <casper@gmail.com>
2019-12-09 19:24:53 +0200

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var del = require('del'); // Delete Something
+var gulp = require('gulp'); // Gulp
+var gulpSequence = require('gulp-sequence') // Gulp tasks in one task serial&parellel
+var newer = require('gulp-newer') // Gulp newer files only upload
+var scss = require('gulp-sass'); // Gulp SCSS to CSS
+var sourcemaps   = require('gulp-sourcemaps'); // Gulp SourceMap in CSS
+var concat = require('gulp-concat'); // Gulp CSS in One file
+var autoprefixer = require('gulp-autoprefixer'); // Gulp CSS autoprefixer
+var browserSync = require('browser-sync').create(); // Server
+var plumber = require('gulp-plumber'); // Checks Errors In SCSS
+var htmlmin = require('gulp-htmlmin'); // Minimize HTML
+var cssnano = require('gulp-cssnano'); // Minimize CSS
+var minify = require('gulp-minify');   //Minimize JS
+
+gulp.task('insertCSS', function() { 
+	// del('markup/build/index.html');   
+	return gulp.src('markup/src/**/*.html')
+  .pipe(gulp.dest('markup/build/**/*.html'));
+});
+
+
+//Del Build
+gulp.task('del_build', function() {
+	del('markup/build');
+});
+
+//Task Create CSS from SCSS
+gulp.task('scss', function() {
+	return gulp.src('markup/src/**/*.scss')
+	.pipe(plumber())
+  .pipe(autoprefixer({
+      Browserslist: ['last 4 versions'],
+      cascade: false
+  }))
+	.pipe(sourcemaps.init())
+	.pipe(scss())
+	.pipe(concat('styles.css'))
+	// .pipe(cssnano()) //Mini
+	.pipe(sourcemaps.write('.')) // Dot if sourcemap separate file
+	.pipe(gulp.dest('markup/build/css'));
+});
+
+//Task HTML
+gulp.task('html', function() {
+	return gulp.src('markup/src/**/*.html')
+	.pipe(newer('markup/build/'))
+	//.pipe(htmlmin({ collapseWhitespace: true })) //Mini	
+	.pipe(gulp.dest('markup/build/'));
+})
+
+//Task IMG to Buil
+gulp.task('img', function() {
+	return gulp.src('markup/src/img/**/*.*')
+	.pipe(newer('markup/build/img/'))
+	.pipe(gulp.dest('markup/build/img/'));
+})
+
+//Task JS to Build and Mini
+gulp.task('js', function() {
+	return gulp.src('markup/src/js/**/*.*')
+	.pipe(newer('markup/build/js/'))
+	.pipe(minify({
+    ext:{
+      src:'-debug.js',
+      min:'.js'
+    }}))
+	.pipe(gulp.dest('markup/build/js/'));
+})
+
+//Task Fonts To Build
+gulp.task('fonts', function() {
+	return gulp.src('markup/src/fonts/**/*.*')
+	.pipe(newer('markup/build/fonts/'))
+	.pipe(gulp.dest('markup/build/fonts/'));
+})
+
+//Task Watch for channges img, scss, js, html
+gulp.task('watchs', function() {
+	gulp.watch('markup/src/img/**/*.*', ['img']);
+	gulp.watch('markup/src/**/*.scss', ['scss']);
+	gulp.watch('markup/src/**/*.js', ['js']);
+	gulp.watch('markup/src/**/*.html', ['html']);
+})
+
+//Task Server
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+       baseDir: "markup/build"
+    }
+  });
+  // gulp.watch("markup/src/**/*.scss", ['scss']);
+  gulp.watch("markup/build/**/*.*").on('change', browserSync.reload);
+});
+
+
+//Run Server width Watch
+gulp.task('watch', gulpSequence('del_build', 'scss', 'html', 'img', 'js', 'fonts', ['watchs','server']))
---
+++ markup/src/js/scripts.js
@@ -0,0 +1,94 @@
+//Fixed navigation
+$(function(){
+  var prevPosition = null;
+
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    if(winTop >= 142){
+      $("body").addClass("sticky-table-header");
+    }else{
+      $("body").removeClass("sticky-table-header");
+      $("body").removeClass("sticky-tabs-header");
+    }
+
+    if (prevPosition > winTop && winTop >= 142) {
+      // console.log(' to the top');
+      $("body").addClass("sticky-tabs-header");
+    } else {
+      $("body").removeClass("sticky-tabs-header");
+    }
+
+    prevPosition = winTop;
+  });
+});
+
+//Search
+$(function() {
+  $(".tabs--header_search-show").click(function(e) {
+    e.preventDefault();
+    $(".tabs--header").toggleClass("show-search");
+  });
+});
+
+$(function() {
+  $(".tabs--header_search-clear").click(function(e) {
+    e.preventDefault();
+    $(".tabs--header").toggleClass("show-search");
+  });
+});
+
+//Actions
+// $(function() {
+//   $(".users--tables .show--actions_btn").click(function(e) {
+//     e.stopPropagation();
+//     $(this).parent().parent().toggleClass("show-actions");
+//   });
+// });
+
+//Filter 
+$(function() {
+  $(".filter_btn").click(function(e) {
+    e.preventDefault();
+    $(".table--list_header").toggleClass("show-filter");
+  });
+});
+
+//Reports More 
+$(function() {
+  $(".report--topic_item-more_btn").click(function(e) {
+    e.preventDefault();
+    $(this).parents('.report--topic_content').toggleClass("show-all");
+  });
+});
+
+//Reports Menu 
+$(function() {
+  $(".report--topic_item-btns").click(function(e) {
+    e.preventDefault();
+    $(this).parents('.report--topic_item').toggleClass("show-menu");
+  });
+});
+
+//Calendar
+$(function() {
+  $(".calendar_btn").click(function(e) {
+    e.preventDefault();
+    $(".table--list_header").toggleClass("show-calendar");
+  });
+  $(".calendar-close_btn").click(function(e) {
+    e.preventDefault();
+    $(".table--list_header").toggleClass("show-calendar");
+  });
+});
+
+//PopUps
+$(document).on('click', '[data-toggle="class"]', function (e) {
+    e.stopPropagation();
+    console.log('CLICK EDIT')
+    var $target = $($(this).data('target'));
+    var classes = $(this).data('classes');
+    $target.toggleClass(classes).toggleClass('show');
+    $('body').toggleClass('overlay');
+    return false;
+});
+
